74191695de6eb6d6fde96d9e38f399627c7d40ed
tylebrad <tylebrad@Isu.edu>
2019-02-18 13:03:35 -0700

    Update RecursiveLinearSearch.java
    10 errors.
    Binary searches next

--- src/main/java/edu/isu/cs/cs3308/algorithms/impl/RecursiveLinearSearch.java
+++ src/main/java/edu/isu/cs/cs3308/algorithms/impl/RecursiveLinearSearch.java
@@ -2,7 +2,7 @@
 // CS 3308
 // 2/13/2019
 // Isaac Griffith
-// Class Description -
+// Class Description - Uses recursion to search an array linearly
 package edu.isu.cs.cs3308.algorithms.impl;
 
 import edu.isu.cs.cs3308.algorithms.ArraySearch;
@@ -10,6 +10,18 @@ import edu.isu.cs.cs3308.algorithms.ArraySearch;
 public class RecursiveLinearSearch implements ArraySearch {
     @Override
     public <E extends Comparable> int search(E[] array, E item) {
-        return 0;
+        if(array == null || item == null)
+            return -1;
+        else
+            return recLinearSearch(array, item, 0);
+    }
+    public <E extends Comparable> int recLinearSearch(E[] array, E item, int index){
+        if (index >= array.length)
+            return -1;
+        else if (array[index] == item)
+            return index;
+        else
+            return recLinearSearch(array, item, index+1);
+
     }
 }
