a034801f238269817d0010903384a226de361fc5
tylebrad <tylebrad@Isu.edu>
2019-02-24 10:56:04 -0700

    Finished implementing psuedo code
    0 errors

--- src/main/java/edu/isu/cs/cs3308/algorithms/impl/RecursiveBinarySearch.java
+++ src/main/java/edu/isu/cs/cs3308/algorithms/impl/RecursiveBinarySearch.java
@@ -10,6 +10,22 @@ import edu.isu.cs.cs3308.algorithms.ArraySearch;
 public class RecursiveBinarySearch implements ArraySearch {
     @Override
     public <E extends Comparable> int search(E[] array, E item) {
-        return 0;
+        if(array == null || item ==null){
+            return -1;
+        }
+        else return recBinarySearch(array, item, 0, array.length -1);
+    }
+
+    public <E extends Comparable> int recBinarySearch(E[] array, E item, int low, int high){
+        int index = (low + high) / 2;
+        if(low >= high)
+            return -1;
+        if (item == array[index])
+            return index;
+        if (item.compareTo(array[index]) < 0)
+            return recBinarySearch(array, item, low, index - 1);
+        else
+            return recBinarySearch(array, item, index + 1, high);
     }
 }
+
